d112078f4a254203fcf9d89c550263e59efb4af6
Anish Kumar Yadav <61132432+yadavAnish@users.noreply.github.com>
2021-07-13 13:50:28 +0530

    Data saved to Local DB  and also retrived data and displayed on mobile screen

--- realmDbProject/App.js
+++ realmDbProject/App.js
@@ -2,114 +2,161 @@ import React,{useEffect,useState} from 'react'
 //import { useForm } from 'react-hook-form';
 import Realm from 'realm';
 
+
+
 import { SafeAreaView, View, FlatList, onsubmit, Text,Input,Image,Card, Dimensions,handleSubmit, Button} from 'react-native';
 import axios from 'axios';
 import { userSchema } from './android/database/schema';
+
+import { CarSchema } from './android/database/schema';
 
  const App=()=>{
   //const { register, handleSubmit, errors } = useForm(); // initialize the hook
   
     const[user,setUser]=useState([])
+    const[retrive,setRet]=useState([])
 
     // console.log(user);
 
     useEffect(() => {
-        getDataUsingSimpleGetCall()
-     
-  savedata()
-       
+       // getDataUsingSimpleGetCall()
+     savedata()
+      //retrive()
        },[]);
 
 
-    function getDataUsingSimpleGetCall () {
-      axios.get('https://jsonplaceholder.typicode.com/posts')
+
+       // get value from Api and saved it to local db
+
+    // function getDataUsingSimpleGetCall () {
+    //   axios.get('https://jsonplaceholder.typicode.com/posts')
             
-                  .then(function(response){
-                    setUser(response.data)
-                   console.log(JSON.stringify(response.data))
+    //               .then(function(response){
+    //                 setUser(response.data)
+    //                //console.log(JSON.stringify(response.data))
+    //               //  Realm.open({
+    //               //   path: "realm.realm",
+    //               //   schema: [userSchema],
+    //               // }).then(realm => {
+    //               //   // Create Realm objects and write to local storage
+    //               //   realm.write(() => {
+    //               //     response.data.forEach(obj => {
+    //               //       realm.create('user', obj);
+    //               //     })
+    //               //     console.log("data inserted")
+                     
+    //               //     }) 
+    //               //   })
 
                  
-            })
-            .catch(function(error){
-                console.log(error)
-            })
-          }
+    //         })
+    //         .catch(function(error){
+    //             console.log(error)
+    //         })
+    //       }
 
 
+         
+          
 
+// AnOther method to save local db
         function savedata(){
-            Realm.open({
-              path: "realm.realm",
-              schema: [userSchema],
-            }).then(realm => {
-              // Create Realm objects and write to local storage
-              realm.write(() => {
-                user.forEach(obj => {
-                  realm.create(user, obj);
-                })
-                console.log("data inserted")
-               
-                }) 
+          Realm.open({
+            path: "newrealm.realm",
+            schema: [userSchema],
+          }).then(realm => {
+            // Create Realm objects and write to local storage
+            realm.write(() => {
+              user.forEach(obj => {
+                realm.create('user', obj);
               })
+              console.log("data inserted")
+             
+              }) 
+
+            //realm.objects is used to retrive data from local db
+              let userss = realm.objects('user');
+               // Local Db datas are assiged in userss and it is set to retrive
+              setRet(userss)
+              //console.log(userss);
+              console.log("=====================================================================")
+
+             // realm.close();
+            })
+    
           }
+
+
+
+          //insert into object
+
+  //       Realm.open({
+  //         schema: [CarSchema],
+  //       }).then(realm => {
+  //   // Create Realm objects and write to local storage
+  //   realm.write(() => {
+  //     const myCar = realm.create('Car', {
+  //       make: 'bmw',
+  //       model: 'Civic',
+  //       miles: 1000,
+  //     });
+  //     myCar.miles += 20; // Update a property value
+  //   });
+    
+  // })
+
+
+
+
+
+  //retrive function
+  // Realm.open({
+  //           schema: [CarSchema],
+  //         }).then(realm => {
+  //           let cars=realm.objects('Car')
+  //           console.log(cars)
+  //         })
+
+
+
+
+
+
+
+// function retrive(){
+//   // Realm.open({
+//   //   schema: [userSchema],
+ 
+//   // })
+//   (realm=>{
+  
+//     console.log("===================================================================================================================")
+//     let usersss = realm.objects('user');
+//               console.log(usersss);
+//     console.log("=====================================users=================================")
+    
+    
+//  })
+  
+// }     
+
         
-         // realm.close();
-
-
-        // if(!user){
-        //     return null
-        // }
          
     return(
       <View style={{ backgroundColor: '#FFFFFF'}}>
      
           <Text >User</Text>
-       
-
-
-          {/* <TextInput
-           onChangeText={setMovie}
-           value={movie}
-        />
-
-        <Button title="Submit" onPress={handleSubmit(onsubmit )} />
-
-           */}
-           {/* <FlatList data={movie}
+      
+           <FlatList data={retrive}
             keyExtractor={({ id }, index) => id}
             renderItem={({ item }) => (
-            <Text>{item.id + '. ' + item.name}</Text>
+            <Text>{item.id + '. ' + item.title + '. ' + item.body}</Text>
                             )}
-                
-          /> */}
-          {/* <div>
-          data={movie}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-           <h1> {item.id + '. ' + item.name}</h1>
-                            )}
-
-          </div> */}
-{/* 
-<div>
-
-<form onSubmit={handleSubmit(onSubmit)}>    
-<Input type="submit" /> 
-
-      <div>
-       { data={movie}}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-           <Text> {item.id + '. ' + item.name}</Text>
-                            )}
-
-          </div>
-
-    </form>
-    </div> */}
+/>    
         </View>
-        
     )
             }
+        
+    
 
 export default App
